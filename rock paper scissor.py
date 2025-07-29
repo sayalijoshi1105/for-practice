e875aef8cae14ab49e02fc83e9d576f9866d0c2d
sayalijoshi1105 <sayalijoshi694@gmail.com>
2025-07-29 16:28:26 +0530

    added rock paper scissor.py

---
+++ rock paper scissor.py
@@ -0,0 +1,18 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+user = input("Choose rock, paper, or scissors: ").lower()
+computer = random.choice(choices)
+
+print("You chose:", user)
+print("Computer chose:", computer)
+
+if user == computer:
+    print("It's a tie!")
+elif (user == "rock" and computer == "scissors") or \
+     (user == "scissors" and computer == "paper") or \
+     (user == "paper" and computer == "rock"):
+    print("You win!")
+else:
+    print("You lose!")
