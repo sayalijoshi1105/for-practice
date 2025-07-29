82e25f618a3f7063c5f41cfcdd24b061666a4b1e
Sayali Joshi <sayalijoshi694@gmail.com>
2025-07-29 16:30:02 +0530

    Update rock paper scissor.py

--- rock paper scissor.py
+++ rock paper scissor.py
@@ -8,11 +8,3 @@ computer = random.choice(choices)
 print("You chose:", user)
 print("Computer chose:", computer)
 
-if user == computer:
-    print("It's a tie!")
-elif (user == "rock" and computer == "scissors") or \
-     (user == "scissors" and computer == "paper") or \
-     (user == "paper" and computer == "rock"):
-    print("You win!")
-else:
-    print("You lose!")
